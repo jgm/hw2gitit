fb61a4a38eb05afd3b618e70278e764403d4face
John MacFarlane <fiddlosopher@gmail.com>
2012-05-19 11:13:28 -0700

    removeDoubleDots - also dot at end of pagename.
    This would combine with .page to form a doubledot.

--- hw2gitit.hs
+++ hw2gitit.hs
@@ -113,11 +113,12 @@ fromUrl = fromUrlString . decodeString . unEscapeString . takeWhile (/='?')
 
 -- filestore can't deal with ? and * in filenames
 fromUrlString :: String -> String
-fromUrlString = removeDoubleDots .
+fromUrlString =
   unwords . words . strip . filter (\c -> c /='?' && c /='*') . ulToSpace
 
 removeDoubleDots :: String -> String
 removeDoubleDots ('.':'.':xs) = '.':removeDoubleDots xs
+removeDoubleDots ['.'] = []
 removeDoubleDots (x:xs) = x:removeDoubleDots xs
 removeDoubleDots [] = []
 
